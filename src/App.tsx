57a5997b25df0f322105b89f6bf9c4dc7bbfb20a
Zoltanbi <zoltanbiro001@gmail.com>
2021-11-08 18:12:09 -0800

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.9/css/boxicons.min.css';
 import './App.scss';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import AppRoutes from './config/AppRoutes';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <AppRoutes/>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
